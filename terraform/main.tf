15af5ea110e404ef4e291976ea7fd6690c500368
Juliet Rodriguez <juliet.rodriguez.1122@gmail.com>
2026-05-24 08:08:13 -0400

    improve S3 governance controls and security

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,11 @@ resource "aws_s3_bucket" "log_bucket" {
     Purpose     = "access-logs"
   }
 }
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.secure_bucket.id
 
-
-
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
