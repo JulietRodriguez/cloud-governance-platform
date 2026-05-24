c35d4db21480902d6cd32c6f17506bf662051512
Juliet Rodriguez <juliet.rodriguez.1122@gmail.com>
2026-05-24 15:08:15 -0400

    finalize governance compliance documentation

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,3 +132,101 @@ resource "aws_iam_policy" "s3_governance_policy" {
     Governance  = "least-privilege"
   }
 }
+resource "aws_s3_bucket" "config_bucket" {
+  bucket = "cloud-governance-config-jr-001"
+
+  tags = {
+    Project     = "cloud-governance-platform"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+    Purpose     = "aws-config"
+  }
+}
+resource "aws_iam_role" "config_role" {
+  name = "aws-config-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "config.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "config_policy_attach" {
+  role       = aws_iam_role.config_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
+}
+resource "aws_config_configuration_recorder" "config_recorder" {
+  name     = "governance-config-recorder"
+  role_arn = aws_iam_role.config_role.arn
+
+  recording_group {
+    all_supported = true
+  }
+}
+resource "aws_config_delivery_channel" "config_delivery" {
+  name           = "governance-config-delivery"
+  s3_bucket_name = aws_s3_bucket.config_bucket.bucket
+
+  depends_on = [
+    aws_config_configuration_recorder.config_recorder
+  ]
+}
+resource "aws_config_config_rule" "s3_bucket_public_read_prohibited" {
+  name = "s3-bucket-public-read-prohibited"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_PUBLIC_READ_PROHIBITED"
+  }
+
+  depends_on = [
+    aws_config_configuration_recorder.config_recorder
+  ]
+}
+resource "aws_s3_bucket_policy" "config_bucket_policy" {
+  bucket = aws_s3_bucket.config_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSConfigBucketPermissionsCheck"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "config.amazonaws.com"
+        }
+
+        Action = "s3:GetBucketAcl"
+
+        Resource = aws_s3_bucket.config_bucket.arn
+      },
+      {
+        Sid    = "AWSConfigBucketDelivery"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "config.amazonaws.com"
+        }
+
+        Action = "s3:PutObject"
+
+        Resource = "${aws_s3_bucket.config_bucket.arn}/AWSLogs/719152986612/Config/*"
+
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
