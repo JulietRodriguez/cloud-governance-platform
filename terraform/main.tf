d72eec2ae84173457774b28bab6f8f57f4f1fcc5
Juliet Rodriguez <juliet.rodriguez.1122@gmail.com>
2026-05-24 14:22:01 -0400

    add CloudTrail governance monitoring evidence

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,3 +47,59 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+resource "aws_s3_bucket" "cloudtrail_bucket" {
+  bucket = "cloud-governance-cloudtrail-jr-001"
+
+  tags = {
+    Project     = "cloud-governance-platform"
+    Environment = "dev"
+    Owner       = "JulietRodriguez"
+    ManagedBy   = "Terraform"
+    Purpose     = "cloudtrail-logs"
+  }
+}
+resource "aws_cloudtrail" "governance_trail" {
+  name                          = "governance-trail"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.id
+  include_global_service_events = true
+  is_multi_region_trail         = true
+
+  tags = {
+    Project     = "cloud-governance-platform"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
+resource "aws_s3_bucket_policy" "cloudtrail_policy" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.cloudtrail_bucket.arn
+      },
+      {
+        Sid    = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = "${aws_s3_bucket.cloudtrail_bucket.arn}/AWSLogs/719152986612/*"
+
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
